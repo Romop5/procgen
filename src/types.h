5b279238c968283b32a95029536dd1c17d988736
Romop5 <rom.dobias@gmail.com>
2017-10-14 21:17:05 +0200

    Almost there for this week :D

--- src/types.h
+++ src/types.h
@@ -8,4 +8,13 @@
 	FNC(double,"double");\
 	FNC(char,"char");
 
+#define FORALL_ATOMICTYPES3(FNC,PARAM)\
+	FNC(int,"int",PARAM);\
+	FNC(bool,"bool",PARAM);\
+	FNC(short,"short",PARAM);\
+	FNC(long,"long",PARAM);\
+	FNC(float,"float",PARAM);\
+	FNC(double,"double",PARAM);\
+	FNC(char,"char",PARAM);
+
 
--- test/variablereg.cpp
+++ test/variablereg.cpp
@@ -18,13 +18,41 @@ int main()
 
 	auto fr = FunctionReg(tr);	
 
-	#define REG_FUNC_FORALL(funcname)\
-		FORALL_ATOMICTYPES(REG_TYPE);
-	#define REG_FUNC(type,typeName)\
-		fr.addFunction(##funcname#typeName,[]{return std::make_shared<funcname<type>>()});
+	#define REG_FUNC_FORALL(funcname,func)\
+		FORALL_ATOMICTYPES3(REG_FUNC,func);\
+		;
+	#define REG_FUNC(type,typeName,func)\
+		fr.addFunction(#func ":" typeName,\
+		[]{return std::static_pointer_cast<Function>(std::make_shared<func<type>>());});
+
 	
-	REG_FUNC_FORALL(tAdd);
+	REG_FUNC_FORALL("tAdd",tAdd);
+	//fr.addFunction("addint",[](){return std::static_pointer_cast<Function> (std::make_shared<tAdd<int>>());});
 	std::cout << "Debug" <<std::endl;
 	fr._debug();	
 		
+
+	auto box = fr.getFunc("tAdd:int");
+	
+	vr.addVar("a", tr->sharedResource("int"));
+	vr.addVar("b", tr->sharedResource("int"));
+	vr.addVar("result", tr->sharedResource("int"));
+
+	auto a = vr.getVar("a");
+	auto b = vr.getVar("b");
+	auto result = vr.getVar("result");
+
+	*(int*) a->getData() = 100;
+	*(int*) b->getData() = 5;
+
+	box->inputs.push_back(a);
+	box->inputs.push_back(b);
+	box->output = result;
+
+	(*box)();
+
+	int r = *(int*) result->getData();
+	std::cout << "result" << r << std::endl;
+	
+	
 }
