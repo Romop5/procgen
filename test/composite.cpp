9d65e58ea1481417f726e85cca685019b78ea54a
Roman Dobias <roman@a02-419a.kn.vutbr.cz>
2017-11-20 10:35:45 +0100

    Added a test for composite types

--- test/composite.cpp
+++ test/composite.cpp
@@ -36,24 +36,69 @@ TEST_CASE("Composite")
 		REQUIRE((*appender)(stat) == false);
 	}
 
-	// Check all items
-	for(int i = 0; i < 5; i++)
-	{
-		REQUIRE(*(int*) collection->at(i)->getData() == i);
-		std::cout << "Coll: " << i << " - " << *(int*) collection->at(i)->getData() << std::endl;
-	}
+
+	// create structure
+	
+	TypeId intType = tr->getTypeId("int");
+	TypeId floatType  = tr->getTypeId("float");
+
+
+	std::vector<TypeId> types = {intType, floatType};
+
+	auto structType = tr->addComposite("struct test", types);
+	REQUIRE(structType != 0);
+
+	auto structVal = tr->sharedResource("struct test");
+	REQUIRE(structVal != nullptr);
+
 
 	auto index = tr->sharedResource("int");
-	auto getter = std::make_shared<CollectionIndex>();	
-	getter->bindInput(0, fr->getHandler(collection));
-	getter->bindInput(1, fr->getHandler(index));
+	auto intVal = tr->sharedResource("int");
+	auto floatVal = tr->sharedResource("float");
 
-	for(int i = 0; i < 5; i++)
-	{
-		*(int*) index->getData() = i;
-		(*getter)(stat);
-		std::cout << "Coll vol2: " << i << " - " << *(int*) number->getData() << std::endl;
-		REQUIRE(*(int*) getter->getOutput()->getData() == i);
-	}
+	auto setter = std::make_shared<CompositeSet>();
+	setter->bindInput(0,fr->getHandler(structVal));
+	setter->bindInput(1,fr->getHandler(index));
+	setter->bindInput(2,fr->getHandler(intVal));
+
+	/*Set intval */
+	*(int*) index->getData() = 0;
+	*(int*) intVal->getData() = 1337;
+	*(float*) floatVal->getData() = 666.6666f;
+
+	RunStatus rs;
+	REQUIRE((*setter)(rs) == false);
+
+	/* Set float */
+
+	*(int*) index->getData() = 1;
+	setter->bindInput(1,fr->getHandler(index));
+	setter->bindInput(2,fr->getHandler(floatVal));
+
+	REQUIRE((*setter)(rs) == false);
+
+	/* Getter */
+	auto getter = std::make_shared<CompositeGet>();
+	getter->bindInput(0,fr->getHandler(structVal));
+	getter->bindInput(1,fr->getHandler(index));
+
+
+	*(int*) index->getData() = 0;
+	*(int*) intVal->getData() = 0;
+
+	/* Get int val */
+	REQUIRE((*getter)(rs) == false);
+	REQUIRE(*(int*) getter->getOutput()->getData() == 1337);
+	std::cout << "Value of int: " << *(int*) getter->getOutput()->getData() << std::endl;
+
+	/* Get float val */
+	*(int*) index->getData() = 1;
+	REQUIRE((*getter)(rs) == false);
+	REQUIRE(abs(*(float*) getter->getOutput()->getData() - 666.666f) < 1.0f );
+
+	std::cout << "Value of float: " << *(float*) getter->getOutput()->getData() << std::endl;
+
+
+
 
 }
